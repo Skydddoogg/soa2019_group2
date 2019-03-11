660b902777c9700fd92b5ed4dd30f9323f4fd196
lieselottezz <59070141@it.kmitl.ac.th>
2019-03-12 00:58:48 +0700

    Handle error from wrong JSON format

--- api/post/routes/post.route.js
+++ api/post/routes/post.route.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+// var expressValidator = require('express-validator');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
+
 var PostController = require('../controllers/post.controller');
 
 // Create new post
-router.post('/create', PostController.post_create);
+router.post('/create', PostController.validate('postCreate'), PostController.postCreate);
+// router.post('/create', PostController.postCreate);
+
+router.get('/', function (req, res) {
+  res.status(200).json({ message:'hello world' });
+});
 
 module.exports = router;
