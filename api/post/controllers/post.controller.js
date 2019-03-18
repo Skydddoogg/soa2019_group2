ef10e7c32d89caef18a6fb9e6960e8e66433d799
lieselottezz <59070141@it.kmitl.ac.th>
2019-03-18 21:01:56 +0700

    Add new POST Method testing

--- api/post/controllers/post.controller.js
+++ api/post/controllers/post.controller.js
@@ -1,54 +1,44 @@
 const Post = require('../models/post.model');
-const { body } = require('express-validator/check')
+const { check, validationResult } = require('express-validator/check');
 
 exports.postCreate = (req, res) => {
-  req.getValidationResult()
-  .then(validationHandler(res))
-  .then(() => Post.create(req.body))
-  .then((post) => res.status(201).json({result: 'post created'}))
-  .catch(() => res.status(400).json({result: 'post not created'}))
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ result: 'post not created', errors: errors.mapped() });
+  }
+  Post.create(req.body)
+  .then(post => res.status(201).json({ result: 'post created', detail: post }));
 }
 
 exports.postEdit = (req, res) => {
-  req.getValidationResult()
-  .then(validationHandler(res))
-  .then(() => Post.findByIdAndUpdate(req.params.id, req.body, {new: true}).exec())
-  .then((post) => res.status(200).json({result: 'post updated'}))
-  .catch(() => res.status(400).json({result: 'post not updated'}))
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ result: 'post not updated', errors: errors.mapped() });
+  }
+  Post.findByIdAndUpdate(req.params.id, req.body, {new: true}).exec()
+  .then(post => res.status(200).json({ result: 'post updated', detail: post }));
 }
 
 exports.postDelete = (req, res) => {
   Post.findByIdAndDelete(req.params.id)
-  .then((post) => res.status(200).json({result: 'post deleted'}))
-  .catch(() => res.status(400).json({result: "post not deleted"}))
+  .then((post) => res.status(200).json({ result: 'post deleted' }))
+  .catch(err => res.status(400).json({ result: "post not deleted", errors: err }));
 }
 
 exports.validate = (method) => {
   switch (method) {
     case 'createAndUpdate': {
       return [
-        body('subject', "subject doesn't exists").exists({ checkFalsy: true }),
-        body('level', "level doesn't exists").exists({ checkFalsy: true }),
-        body('startTime', "startTime doesn't exists").exists({ checkFalsy: true }),
-        body('endTime', "endTime doesn't exists").exists({ checkFalsy: true }),
-        body('location', "location doesn't exists").exists({ checkFalsy: true }),
-        body('expectPrice', "expectPrice doesn't exists").exists({ checkFalsy: true }),
-        body('detail', "detail doesn't exists").exists({ checkFalsy: true }),
-        body('creatorId', "creatorId doesn't exists").exists({ checkFalsy: true }),
-        body('creatorUsername', "creatorUsername doesn't exists").exists({ checkFalsy: true }),
-        body('creatorType', "creatorType doesn't exists").exists({ checkFalsy: true })
+        check('subject', "subject doesn't exists").exists({ checkFalsy: true }),
+        check('level', "level is wrong format or doesn't exist").isIn(['elementary', 'lower-secondary', 'upper-secondary']),
+        check('startTime', "startTime doesn't exists").exists({ checkFalsy: true }),
+        check('endTime', "endTime doesn't exists").exists({ checkFalsy: true }),
+        check('location', "location doesn't exists").exists({ checkFalsy: true }),
+        check('expectPrice', "expectPrice is wrong format or doesn't exist").exists({ checkFalsy: true }).isNumeric(),
+        check('creatorId', "creatorId doesn't exists").exists({ checkFalsy: true }),
+        check('creatorUsername', "creatorUsername doesn't exists").exists({ checkFalsy: true }),
+        check('creatorType', "creatorType is wrong format or doesn't exist").isIn(['student', 'tutor'])
       ]
     }
-  }
-}
-
-const validationHandler = next => result => {
-  if (result.isEmpty()) {
-    return;
-  }
-  if (!next) {
-    throw new Error(result.array().map(i => `'${i.param}' has ${i.msg}`).join(' '));
-  } else {
-    return next(new Error(result.array().map(i => `'${i.param}' has ${i.msg}`).join('')));
   }
 }
--- api/post/tests/post.test.js
+++ api/post/tests/post.test.js
@@ -11,11 +11,11 @@ afterAll(() => {
 });
 
 /**
- * Testing POST Method with correct data format to add post
+ * Testing POST Method
  */
-describe('POST with correct data format', () => {
+describe('POST with several data format', () => {
 
-  let data = {
+  let data1 = {
     "subject": "math",
     "level": "lower-secondary",
     "startTime": "13.00",
@@ -28,48 +28,106 @@ describe('POST with correct data format', () => {
     "creatorType": "student"
   }
 
-  it('should respond with 201 created', (done) => {
-    request(server)
-    .post('/post/create')
-    .send(data)
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(201)
-    .end((err) => {
-      if (err) return done(err);
-      done();
-    });
-  });
-});
-
-/**
- * Testing POST Method with wrong data format to add post
- */
-describe('POST with wrong data format', () => {
-
-  let data = {
-    // Request with no subject
+  let data2 = {
+    // No subject
     "level": "lower-secondary",
     "startTime": "13.00",
     "endTime": "17.00",
     "location": "Seacon Square",
-    "expectPrice": "500",
+    // No expectPrice
     "detail": "detail for student post",
     "creatorId": "0123456789",
     "creatorUsername": "riariaria",
     "creatorType": "student"
   }
 
-  it('should respond with 400 not created', (done) => {
-    request(server)
-    .post('/post/create')
-    .send(data)
-    .set('Accept', 'application/json')
+  let data3 = {
+    "subject": "data-structure",
+    // Wrong type of level
+    "level": "super-secondary",
+    "startTime": "13.00",
+    "endTime": "17.00",
+    "location": "Seacon Square",
+    // Wrong type of expectPrice
+    "expectPrice": "xyz",
+    "detail": "detail for student post",
+    "creatorId": "0123456789",
+    "creatorUsername": "riariaria",
+    // Wrong type of creatorType
+    "creatorType": "student"
+  }
+
+  // Testing with correct data format
+  it('should respond with 201 Created for correct data format', () => {
+    request(server).post('/post/create').send(data1).set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(201)
+    .then((res) => {
+      const { result, detail } = res.body;
+      expect(result).toBe('post created');
+    })
+    .catch(err => console.log(err));
+  });
+
+  // Testing with incorrect data format (Missing required property)
+  it('should respond with 400 Bad Request for incorrect data format (Missing required property)', () => {
+    request(server).post('/post/create').send(data2).set('Accept', 'application/json')
     .expect('Content-Type', /json/)
     .expect(400)
-    .end((err) => {
-      if (err) return done(err);
-      done();
-    });
+    .then((res) => {
+      const { result, errors } = res.body;
+      expect(result).toBe('post not created');
+      expect(errors.subject.msg).toBe('subject doesn\'t exists');
+      expect(errors.expectPrice.msg).toBe('expectPrice is wrong format or doesn\'t exist');
+    })
+    .catch(err => console.log(err));
   });
+
+  // Testing with incorrect data format (Incorrected property)
+  it('should respond with 400 Bad Request for incorrect data format (Incorrected property)', () => {
+    request(server).post('/post/create').send(data2).set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .then((res) => {
+      const { result, errors } = res.body;
+      expect(result).toBe('post not created');
+      expect(errors.subject.msg).toBe('subject doesn\'t exists');
+      expect(errors.expectPrice.msg).toBe('expectPrice is wrong format or doesn\'t exist');
+      expect(errors.creatorType.msg).toBe('creatorType is wrong format or doesn\'t exist');
+    })
+    .catch(err => console.log(err));
+  });
+  
 });
+
+/**
+ * Testing PUT Method
+ */
+// describe('PUT with correct data format', () => {
+
+//   let data = {
+//     "subject": "science",
+//     "level": "lower-secondary",
+//     "startTime": "13.00",
+//     "endTime": "17.00",
+//     "location": "Seacon Square",
+//     "expectPrice": "500",
+//     "detail": "detail for student post",
+//     "creatorId": "0123456789",
+//     "creatorUsername": "riariaria",
+//     "creatorType": "student"
+//   }
+
+//   it('should respond with 400 not created', (done) => {
+//     request(server)
+//     .post('/post/create')
+//     .send(data)
+//     .set('Accept', 'application/json')
+//     .expect('Content-Type', /json/)
+//     .expect(400)
+//     .end((err) => {
+//       if (err) return done(err);
+//       done();
+//     });
+//   });
+// });
