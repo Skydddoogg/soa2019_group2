c9114257b282287813d9d9f6ce478faf5e0e85f8
lieselottezz <59070141@it.kmitl.ac.th>
2019-03-26 16:34:52 +0700

    Fix Dockerfile, docker-compose TODO: Fix test for post service, readme.md

--- service/search/conf/db.js
+++ service/search/conf/db.js
@@ -2,7 +2,15 @@ require('module-alias/register');
 require('@conf/config');
 
 const mongoose = require('mongoose');
-
 const connectionString = `mongodb://${global.gConfig.db_host}:${global.gConfig.db_port}/${global.gConfig.db_name}`;
-console.log(connectionString);
-mongoose.connect(connectionString, { useNewUrlParser: true, useFindAndModify: false });
+
+var connectWithRetry = function() {
+  console.log('Search service is connecting database at: ' + connectionString);
+  return mongoose.connect(connectionString, { useNewUrlParser: true, useFindAndModify: false }, function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec ', err);
+      setTimeout(connectWithRetry, 5000);
+    }
+  });
+};
+connectWithRetry();
