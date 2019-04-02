c9dc45a178dd9ad396af1d0fa8a71a20decf7bef
lieselottezz <59070141@it.kmitl.ac.th>
2019-04-03 00:15:27 +0700

    Fix controller in post service (replace callback function by async await)

--- service/post/app/api/post/index.js
+++ service/post/app/api/post/index.js
@@ -2,8 +2,8 @@ const Router = require('express').Router;
 const Controller = require('./post.controller');
 const router = new Router();
 
-router.post('/create', Controller.validate('createAndUpdate'), Controller.postCreate);
-router.put('/update/:id', Controller.validate('createAndUpdate'), Controller.postUpdate);
+router.post('/create', Controller.postCreate);
+router.put('/update/:id', Controller.postUpdate);
 router.delete('/delete/:id', Controller.postDelete);
 
 module.exports = router;
--- service/post/server.js
+++ service/post/server.js
@@ -4,13 +4,11 @@ require('@conf/db');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
 
 const port = global.gConfig.port;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator());
 
 require('./app/routes')(app);
 
