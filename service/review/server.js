c6bda91bd12309dacc2c5945b46479bad3e7bd2e
Kykung <59070071@it.kmitl.ac.th>
2019-04-07 19:24:37 +0700

    Fix uncomposable docker containers

--- service/review/server.js
+++ service/review/server.js
@@ -1,17 +1,18 @@
+require('custom-env').env(process.env.NODE_ENV || 'development')
 require('./conf/db');
 require('module-alias/register')
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const PORT = process.env.PORT || 3004;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/routes')(app);
 
-app.listen(port);
-console.log('Review service is listening on port: ' + port);
+app.listen(PORT);
+console.log('Review service is listening on port: ' + PORT);
 
 module.exports = app;
