76f00578c27d86223bebdd8ee997a1649926eaa6
Kykung <59070071@it.kmitl.ac.th>
2019-04-25 12:35:46 +0700

    Edit profile model

--- service/profile/app/api/profile/profile.model.js
+++ service/profile/app/api/profile/profile.model.js
@@ -7,7 +7,17 @@ const ProfileSchema = new mongoose.Schema({
   firstname: {type: String, required: true},
   lastname: {type: String, required: true},
   email: {type: String, required: true},
-  phoneNumber: {type: String, required: true}
+  phoneNumber: {type: String, required: true},
+  nickname: {type: String, required: true},
+  profileImg: {type: String, required: true},
+  highSchool: {type: String, required: true},
+  bachelor: {type: String, required: true},
+  master: {type: String, required: true},
+  doctoral: {type: String, required: true},
+  majorInBachelor: {type: String, required: true},
+  majorInMaster: {type: String, required: true},
+  majorInDoctoral: {type: String, required: true},
+  majorInHighSchool: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Profile', ProfileSchema);
