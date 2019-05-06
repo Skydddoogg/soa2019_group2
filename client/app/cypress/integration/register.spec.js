5cedabfb03409c8a58a9904b467f76edabb2e0a7
Mewkybar <mewloft@gmail.com>
2019-05-06 20:51:03 +0700

    install cypress

---
+++ client/app/cypress/integration/register.spec.js
@@ -0,0 +1,25 @@
+describe("Register Page", () => {
+    beforeEach(function(){
+        cy.visit("localhost:3000/signup")
+    });
+
+
+    it("Success register", () => {
+        cy.get('[data-cy=username]').click()
+     
+    });
+
+    it("", () => {
+       
+    });
+
+    it("", () => {
+       
+    });
+
+    it("", () => {
+       
+    });
+
+
+  });
