8b04f5493cbca9caadc317ee45f7ce858a6d6d71
Mewkybar <mewloft@gmail.com>
2019-05-07 11:51:44 +0700

    Add test TutorFlow

--- client/app/cypress/integration/tutorflow.spec.js
+++ client/app/cypress/integration/tutorflow.spec.js
@@ -1,11 +1,17 @@
-describe("User flow", () =>{
+describe("Tutor flow", () =>{
     
-    it("User login to filter a post", () =>{
-       
+    it("Tutor login to offer ", () =>{
+        cy.visit("localhost:3000")
+        cy.get('[data-cy=loginUsername').type('Saiparn')
+        cy.get('[data-cy=loginPassword').type('Saiparn1')
+        cy.get('[data-cy=loginLogin_btn').click()
+        cy.contains('กำลังเข้าสู่ระบบ...').should('be.visible')
+        cy.wait(500)
+        cy.get('[data-cy=navLogin_btn').click()
 
 
     })
-    it("User login to create a post ", () =>{
+    it("", () =>{
    
     })
     
